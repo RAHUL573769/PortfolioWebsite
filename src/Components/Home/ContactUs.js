a6ce2405b1eb87d0d1a0b2bb9a2831cdb345cc1a
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-06-09 16:59:28 +0600

    forms linkadded

--- src/Components/Home/ContactUs.js
+++ src/Components/Home/ContactUs.js
@@ -18,35 +18,10 @@ const ContactUs = () => {
           <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div class="card-body">
               <div>
-                {/* <form action="https://formsubmit.co/el/pofoxe" method="POST">
-                  <label class="label">
-                    <span class="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="email"
-                    class="input input-bordered"
-                  />
-                  <label class="label">
-                    <span class="label-text">Name</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="password"
-                    class="input input-bordered"
-                  />
-                  <label class="label">
-                    <a href="#" class="label-text-alt link link-hover">
-                      Forgot password?
-                    </a>
-                  </label>
-                  <div class=" mt-6">
-                    <button class="btn btn-primary">Login</button>
-                  </div>
-                </form> */}
-
-                <button>
-                  <a href="https://formsubmit.co/el/pofoxe">Click</a>
+                <button class="btn btn-primary">
+                  <a href="https://formsubmit.co/el/pofoxe">
+                    Click Here To Stay in Touch
+                  </a>
                 </button>
               </div>
             </div>
